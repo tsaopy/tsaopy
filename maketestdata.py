a211a20410535f26efd211f2dda023320b449d0c
charly1256 <94293518+charly1256@users.noreply.github.com>
2021-11-16 13:02:25 -0300

    Update maketestdata.py

--- maketestdata.py
+++ maketestdata.py
@@ -1,57 +1,53 @@
-##############################################################################
-#                                 PRELIMINARY
-##############################################################################
-
-# def functions
+# ### imports
 import numpy as np
-def _deriv(X_,A_,B_):
-    x_,v_ = X_[0],X_[1]
-    a_ = 0
-    for j in range(len(A_)):
-        a_ = a_ - A_[j]*v_*(abs(v_))**j
-    for j in range(len(B_)):
-        a_ = a_ - B_[j]*x_**(j+1)    
-    return np.array([v_,a_])
+import matplotlib.pyplot as plt
 
-def _rk4(h,X_):
-    k1 = h*_deriv(X_)
-    k2 = h*_deriv(X_+0.5*k1)
-    k3 = h*_deriv(X_+0.5*k2)
-    k4 = h*_deriv(X_+k3)
-    return (X_+(k1+k4+2*k2+2*k3)/6)
+from oscadsf2py import simulation  # integrad numerico importado x f2py
 
-### make data
-np.random.seed(33525)
+# ajustar semilla de np random
+np.random.seed(12345)
 
-# noise gen function
-u_noise,n_noise = 1e-2,2e-2
-noise = lambda : np.random.normal(0,n_noise)+np.random.uniform(-u_noise,u_noise)
+# funcion auxiliar para agregar ruido a los datos
+u_noise, n_noise = 1e-2, 2e-2
 
-# simu
-A,B = [0.0],[1.0,0.0,0.5]
-dt,x0 = 1e-4,1.0+noise()
-t,x,X,E = [0],[x0],np.array([x0,0]),np.sum(B)
-tf = 30
 
-_deriv_coefs = lambda X_ : _deriv(X_,A,B)
+def noise():
+    return np.random.normal(0, n_noise)\
+           + np.random.uniform(-u_noise, u_noise)
 
-def _rk4_c(h,X_):
-    k1 = h*_deriv_coefs(X_)
-    k2 = h*_deriv_coefs(X_+0.5*k1)
-    k3 = h*_deriv_coefs(X_+0.5*k2)
-    k4 = h*_deriv_coefs(X_+k3)
-    return (X_+(k1+k4+2*k2+2*k3)/6)
 
-while t[-1]<tf:
-    t.append(t[-1]+dt)
-    X = _rk4_c(dt,X)
-    x.append(X[0]+noise())
+# ### ajustar parametros de la simulacion
+# tf : tiempo final, A : coefs de la fuerza de damping, B : coefs de
+# la fuerza conservativa, h : paso de integracion
+tf, A, B, h = 30, [0.0], [1.0, 0.0, 0.5], 1e-4
+na, nb, n_data = len(A), len(B), int(tf/h)+1
 
-t,x = t[::300],x[::300]
-np.savetxt('testdata.txt',[[t[_],x[_]] for _ in range(len(t))])
-    
-from matplotlib import pyplot as plt
+# ### hacer la simulacion
+# condiciones iniciales
+x0, v0 = 1.0+noise(), 0.0+noise()
+# hacer la simulacion y darle
+x, t = simulation([x0, v0], A, B, h, n_data, na, nb),\
+       np.linspace(0, tf, n_data)
 
-plt.figure(figsize=(7,5),dpi=150)
-plt.plot(t,x,color='tab:red')
+# ### definir largo de salida de la serie de datos y agregar ruido
+# definir largo de output
+n_out = 100
+
+# recortar la serie para que tenga el largo n_out
+step = n_data//n_out
+x, t = x[::step], t[::step]
+while len(x) > n_out:
+    x, t = x[:-1], t[:-1]
+
+# agregar ruido
+for _ in range(len(x)):
+    x[_] = x[_] + noise()
+
+# ### finalizacion
+# ver esquema de los datos
+plt.figure(figsize=(7, 5), dpi=150)
+plt.plot(t, x, color='tab:red')
 plt.show()
+
+# guardar los datos
+np.savetxt('testdata.txt', [[t[_], x[_]] for _ in range(len(t))])
