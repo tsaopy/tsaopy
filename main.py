e8c461ee389ea47264733cafd3e040e40f2fd137
sofi1256 <94293518+sofi1256@users.noreply.github.com>
2021-12-28 17:59:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#######################################################################
-#                           setup preliminar
-#######################################################################
+##############################################################################
+#                               setup preliminar
+##############################################################################
 
 # ### imports
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import minimize,basinhopping
+from scipy.optimize import basinhopping
 from multiprocessing import Pool, cpu_count
 from time import time
 
@@ -19,19 +19,21 @@ from oscadsf2py import simulation
 filename = 'testdata.txt'
 data_t, data_x = np.loadtxt(filename, usecols=0),\
     np.loadtxt(filename, usecols=1)
-
+    
 # incerteza 1 sigma sobre los datos x importados
-data_sigma = 2e-2
-
+data_sigma = 0.25
+    
 # obtener parametros basicos
 t_step, t0, tf, N_data, x0_ini = data_t[1]-data_t[0], data_t[0],\
                          data_t[-1], len(data_x), data_x[0]
 # paso de integracion
-dt = t_step/10.0
+t_split = 4
+dt = t_step/t_split
 
 # ### priors
 # IMPORTANTE : las dimensiones del array de priors define posteriormente
 # la cantidad de terminos de la ec de mov que considera emcee
+
 # estimacion del valor omega0 al cuadrado
 w0sq_guess = 1.0
 w0_guess = w0sq_guess**0.5
@@ -39,7 +41,7 @@ A_guess = [0.0]
 B_guess = [w0sq_guess, 0.0, 0.0]
 
 # construccion del array de priors
-prior = [x0_ini, 0.0, A_guess, B_guess]
+prior = [x0_ini, 0.0, A_guess, B_guess,0.0]
 # estimacion del rango de variabilidad de los parametros
 # la convergencia o no del metodo es altamente sensible de esto
 # elegir bien
@@ -48,11 +50,14 @@ prior_bounds = [  # x0
                 # v0
                 (-x0_ini*w0_guess/10, x0_ini*w0_guess/10),
                 # disipacion
-                (-w0sq_guess/5, w0sq_guess/3),
+                (-w0sq_guess/5, w0sq_guess/2),
                 # potencial
-                (0, 3*w0sq_guess),
+                (-1e-10, 3*w0sq_guess),
                 (-0.5*w0sq_guess, 1*w0sq_guess),
-                (-0.5*w0sq_guess, 1*w0sq_guess)]
+                (-0.5*w0sq_guess, 1*w0sq_guess),
+                # termino del bias
+                (-np.inf,np.inf)
+                ]
 
 
 # ### funciones auxiliares
@@ -61,7 +66,7 @@ prior_bounds = [  # x0
 # y devolver un array con 'info' sobre la forma de los arrays
 # saltea los primeros 2 elem destinados a x0 y v0
 def wrapper_aux_function(p_m):
-    return np.hstack(p_m), [len(_) for _ in p_m[2:]]
+    return np.hstack(p_m), [len(_) for _ in p_m[2:-1]]
 
 
 # tomar el array aplanado + la info y reconstruir el array original
@@ -71,7 +76,7 @@ def dewrap_aux_function(array_elem_, wrap_info_):
         dewrapped_array.append(array_elem_[current_index:(current_index
                                + wrap_info_[id_])])
         current_index = current_index + wrap_info_[id_]
-    return dewrapped_array
+    return dewrapped_array+[array_elem_[-1]]
 
 
 # wrapear los priors anteriores
@@ -80,28 +85,26 @@ na, nb = main_wrap_info
 # definir la dimension del espacio de parametros de emcee
 ndim = len(priors_wraped)
 
-#######################################################################
-#                           setup de emcee
-#######################################################################
+##############################################################################
+#                               setup de emcee
+##############################################################################
 
 
 # definicion del modelo
 def model(_coefs_):
     x0_simu, v0_simu = _coefs_[0], _coefs_[1]
-    _coefs_aux = dewrap_aux_function(_coefs_[2:], main_wrap_info)
+    _coefs_aux = dewrap_aux_function(_coefs_[2:-1], main_wrap_info)
     A_simu, B_simu = _coefs_aux[0], _coefs_aux[1]
-    return simulation([x0_simu, v0_simu], A_simu, B_simu, dt, 10*N_data,
-                      na, nb)[::10]
-
+    return simulation([x0_simu, v0_simu], A_simu, B_simu, dt, t_split*N_data,
+                      na, nb)[::t_split]
 
 # definicion de la func likelihood
 def log_likelihood(_coefs_):
     prediction = model(_coefs_)
-    # esta sentencia devuelve -np.inf si diverge la simulacion
     if np.isnan(prediction[-1]):
           return -np.inf
     ll = -0.5*np.sum(((prediction-data_x)/data_sigma)**2)
-    return (ll)
+    return ll
 
 
 # testeo para q cada parametro del modelo caiga dentro del rango acept
@@ -168,25 +171,25 @@ def main(p0, n_walkers, n_iter, ndim, log_probability):
         return sampler, pos, prob, state
 
 
-#######################################################################
-#                           correr el programa
-#######################################################################
+##############################################################################
+#                               correr el programa
+##############################################################################
 
 start = time()
 sampler, pos, prob, state = main(p0, n_walkers, n_iter, ndim,
                                  log_probability)
 
-#######################################################################
+##############################################################################
 #               extraer resultados del sampler y procesarlos
-#######################################################################
+##############################################################################
 
-fig, axes = plt.subplots(ndim, figsize=(10, 7), sharex=True)
+fig, axes = plt.subplots(ndim-1, figsize=(10, 7), sharex=True)
 samples = sampler.get_chain()
 label_list = ['x0', 'v0'] +\
             ['a'+str(_xd+1) for _xd in range(main_wrap_info[0])] + \
             ['b'+str(_xd+1) for _xd in range(main_wrap_info[1])]
 
-for i in range(ndim):
+for i in range(ndim-1):
     ax = axes[i]
     ax.plot(samples[:, :, i], "k", alpha=0.3)
     ax.set_xlim(0, len(samples))
@@ -197,7 +200,7 @@ axes[-1].set_xlabel("step number")
 
 autoc_tau = int(max(sampler.get_autocorr_time()))
 flat_samples = sampler.get_chain(flat=True, thin=autoc_tau)
-sample_truths = [np.mean(flat_samples[:, _]) for _ in range(ndim)]
+sample_truths = [np.mean(flat_samples[:, _]) for _ in range(ndim-1)]
 
 fig = corner.corner(flat_samples, labels=label_list,
                     quantiles=(0.16, 0.84), show_titles=True,
