b9d2aa82ec1e3c1c512c861c3eecdb61fbc7cb50
Sofía <94293518+sofi1256@users.noreply.github.com>
2022-05-13 18:16:33 -0300

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -8,25 +8,10 @@ data_x_sigma = 0.2
 
 # priors
 
-def x0_prior(x):
-    if x<1.3 and x>0.7:
-        return 1
-    else:
-        return 0.0
+x0_prior = bend.uniform_prior(0.7,1.3)
+a1_prior = bend.uniform_prior(-5.0,5.0)
+b1_prior = bend.uniform_prior(0.0,5.0)
     
-def a1_prior(x):
-    if x<5.0 and x>-5.0:
-        return 1
-    else:
-        return 0.0
-    
-def b1_prior(x):
-    if x<5.0 and x>0.0:
-        return 1
-    else:
-        return 0.0
-    
-
 # parameters
 
 x0 = bend.FittingParameter(1.0,'x0',1,x0_prior)
@@ -40,10 +25,12 @@ parameters = [x0,v0,a1,b1]
 
 model1 = bend.Model(parameters,data_t,data_x,data_x_sigma)
 
-sampler,_,_,_ = model1.setup_sampler(100, 200, 1000)
-flat_samples = sampler.get_chain(flat=True)
+sampler,_,_,_ = model1.setup_sampler(200, 150, 200)
+samples, flat_samples = sampler.get_chain(), sampler.get_chain(flat=True)
 
 # plots
 
 label_list = model1.params_labels
-bend.make_cornerplots(flat_samples,label_list)
+bend.cornerplots(flat_samples,label_list)
+bend.traceplots(samples,label_list)
+bend.autocplots(flat_samples,label_list)
