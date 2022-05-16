759ffe64a9218a800445ccca19d55e5cf0a62b9d
Sofía <94293518+sofi1256@users.noreply.github.com>
2022-05-16 17:48:21 -0300

    Update maketestdata.py

--- maketestdata.py
+++ maketestdata.py
@@ -1,62 +1,56 @@
-# ### imports
 import numpy as np
 import matplotlib.pyplot as plt
+from oscadsf2py import simulationv  # f2py numerical simulation
 
-from scipy.ndimage import gaussian_filter1d
-from oscadsf2py import simulation  # integrad numerico importado x f2py
-
-# ajustar semilla de np random
+# set random seed
 np.random.seed(0o204)
 
-# funcion auxiliar para agregar ruido a los datos
+# aux function to add noise
 u_noise, n_noise = 5e-2, 5e-2
-
-
 def noise():
     return np.random.normal(0, n_noise)\
            + np.random.uniform(-u_noise, u_noise)
 
 
-# ### ajustar parametros de la simulacion
-# tf : tiempo final, A : coefs de la fuerza de damping, B : coefs de
-# la fuerza conservativa, h : paso de integracion
+# ### simulation parameters
+# tf : final time, A : damping coefs, B : potential coefs
+# C : mixed coefs matrix, F : driving force, h : integration timestep
 
 tf, A, B, C, F, h = 50, [0.2], [1.0],\
                     np.array([[0.0],
                               [0.0]]), \
                     [0.0, 0.0, 0.0], 1e-4
 na, nb, cn, cm, n_data = len(A), len(B), len(C[:,0]), len(C[0,:]), int(tf/h)+1
-# ### hacer la simulacion
-# condiciones iniciales
+
+# init conds
 x0, v0 = 1.0, 0.0
-# hacer la simulacion
-x, t = simulation([x0, v0], A, B, C, F, h, n_data, na, nb, cn, cm),\
+# do simulation
+xv_results, t = simulationv([x0, v0], A, B, C, F, h, n_data, na, nb, cn, cm),\
        np.linspace(0, tf, n_data)
+       
+x,v = xv_results[:,0], xv_results[:,1]
 
-# ### definir largo de salida de la serie de datos y agregar ruido
+# ### thin data arrays to given output length
 # definir largo de output
+
 n_out = 1000
-
-# recortar la serie para que tenga el largo n_out
 step = n_data//n_out
-x, t = x[::step], t[::step]
+
+x, v, t = x[::step], v[::step], t[::step]
 while len(x) > n_out:
-    x, t = x[:-1], t[:-1]
+    x, v, t = x[:-1], v[:-1], t[:-1]
 
-# agregar ruido
-for _ in range(1,len(x)):
+# add noise
+for _ in range(len(x)):
     x[_] = x[_] + noise()
-    
-### procesar filtrado
-gf_sd = 9
-x_process = gaussian_filter1d(x, gf_sd)
+    v[_] = v[_] + noise()/1.2
 
-# ### finalizacion
-# ver esquema de los datos
+# plot data
 plt.figure(figsize=(15, 5), dpi=150)
-plt.plot(t, x, lw=0.5)
-plt.plot(t, x_process, color='tab:red')
+plt.plot(t, x, lw=0.5, color = 'tab:red', label='position')
+plt.plot(t, v, color='tab:purple', label='velocity')
+plt.legend()
 plt.show()
 
-# guardar los datos
-np.savetxt('experiment_data.txt', [[t[_], x[_]] for _ in range(len(t))])
+# save data
+np.savetxt('experiment_data.txt', [[t[_], x[_], v[_]] for _ in range(len(t))])
