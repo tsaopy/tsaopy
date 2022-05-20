001ba1cabf445d7defd3eea4cb2938145ac95faf
Sofía <94293518+sofi1256@users.noreply.github.com>
2022-05-20 16:01:39 -0300

    Update solution.py

--- notebook2/solution.py
+++ notebook2/solution.py
@@ -1,23 +1,24 @@
 import numpy as np
+import numpy as np
 import backend as bend
 
 # load data
 data = np.loadtxt('experiment_data.txt')
 data_t,data_x,data_v = data[:,0],data[:,1],data[:,2]
-data_x_sigma,data_v_sigma = 0.4,0.5
+data_x_sigma,data_v_sigma = 0.3,0.4
 
 # priors
 
-x0_prior = bend.uniform_prior(1.5,2.5)
-v0_prior = bend.uniform_prior(-1.0,1.0)
-a1_prior = bend.uniform_prior(-5.0,5.0)
-a2_prior = bend.uniform_prior(-5.0,5.0)
-b1_prior = bend.uniform_prior(0.0,5.0)
-b2_prior = bend.uniform_prior(-5.0,5.0)
-c11_prior = bend.uniform_prior(-5.0,5.0)
-c12_prior = bend.uniform_prior(-5.0,5.0)
-c21_prior = bend.uniform_prior(-5.0,5.0)
-c22_prior = bend.uniform_prior(-5.0,5.0)
+x0_prior = bend.normal_prior(2.0,1.0)
+v0_prior = bend.normal_prior(0.0,1.0)
+a1_prior = bend.normal_prior(0.0,5.0)
+a2_prior = bend.normal_prior(0.0,5.0)
+b1_prior = bend.normal_prior(2.5,5.0)
+b2_prior = bend.normal_prior(0.0,5.0)
+c11_prior = bend.normal_prior(0.0,5.0)
+c12_prior = bend.normal_prior(0.0,5.0)
+c21_prior = bend.normal_prior(0.0,5.0)
+c22_prior = bend.normal_prior(0.0,5.0)
 
 
 # parameters
@@ -26,7 +27,7 @@ x0 = bend.FittingParameter(2.0,'x0',1,x0_prior)
 v0 = bend.FittingParameter(0.0,'v0',1,v0_prior)
 a1 = bend.FittingParameter(0.0, 'a', 1, a1_prior)
 a2 = bend.FittingParameter(0.0, 'a', 2, a2_prior)
-b1 = bend.FittingParameter(0.5,'b',1,b1_prior)
+b1 = bend.FittingParameter(0.0,'b',1,b1_prior)
 b2 = bend.FittingParameter(0.0,'b',2,b2_prior)
 c11 = bend.FittingParameter(0.0,'c',(1,1),c11_prior)
 c12 = bend.FittingParameter(0.0,'c',(1,2),c12_prior)
@@ -41,7 +42,7 @@ parameters = [x0,v0,a1,a2,b1,b2,c11,c12,c21,c22]
 model2 = bend.VelocityModel(parameters,data_t,data_x,data_v,
                             data_x_sigma,data_v_sigma)
 
-sampler,_,_,_ = model2.setup_sampler(200, 300, 300)
+sampler,_,_,_ = model2.setup_sampler(200, 2000, 500)
 samples, flat_samples = sampler.get_chain(), sampler.get_chain(flat=True)
 
 label_list = model2.params_labels
