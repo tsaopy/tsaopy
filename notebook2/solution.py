939b7c337d23f7a1c93745374ea74d64c6294a66
Sofía <94293518+sofi1256@users.noreply.github.com>
2022-05-21 15:37:31 -0300

    Update solution.py

--- notebook2/solution.py
+++ notebook2/solution.py
@@ -8,40 +8,28 @@ data_x_sigma,data_v_sigma = 0.3,0.4
 
 # priors
 
-x0_prior = bend.normal_prior(2.0,100.0)
-v0_prior = bend.normal_prior(0.0,100.0)
-a1_prior = bend.normal_prior(0.0,100.0)
-a2_prior = bend.normal_prior(0.0,100.0)
-b1_prior = bend.normal_prior(0.0,100.0)
-b2_prior = bend.normal_prior(0.0,100.0)
-c11_prior = bend.normal_prior(0.0,100.0)
-c12_prior = bend.normal_prior(0.0,100.0)
-c21_prior = bend.normal_prior(0.0,100.0)
-c22_prior = bend.normal_prior(0.0,100.0)
-
+x0_prior = bend.uniform_prior(1.9,2.1)
+v0_prior = bend.uniform_prior(-0.75,0.75)
+a1_prior = bend.normal_prior(-1.9,2.0)
+b1_prior = bend.normal_prior(0.9,0.3)
+c21_prior = bend.normal_prior(1.6,1.5)
 
 # parameters
 
 x0 = bend.FittingParameter(2.0,'x0',1,x0_prior)
 v0 = bend.FittingParameter(0.0,'v0',1,v0_prior)
-a1 = bend.FittingParameter(0.0, 'a', 1, a1_prior)
-a2 = bend.FittingParameter(0.0, 'a', 2, a2_prior)
-b1 = bend.FittingParameter(0.0,'b',1,b1_prior)
-b2 = bend.FittingParameter(0.0,'b',2,b2_prior)
-c11 = bend.FittingParameter(0.0,'c',(1,1),c11_prior)
-c12 = bend.FittingParameter(0.0,'c',(1,2),c12_prior)
-c21 = bend.FittingParameter(0.0,'c',(2,1),c21_prior)
-c22 = bend.FittingParameter(0.0,'c',(2,2),c22_prior)
+a1 = bend.FittingParameter(-1.9, 'a', 1, a1_prior)
+b1 = bend.FittingParameter(0.9,'b',1,b1_prior)
+c21 = bend.FittingParameter(1.6,'c',(2,1),c21_prior)
 
-
-parameters = [x0,v0,a1,a2,b1,b2,c11,c12,c21,c22]
+parameters = [x0,v0,a1,b1,c21]
 
 # model 2 (velocity)
 
 model2 = bend.VelocityModel(parameters,data_t,data_x,data_v,
                             data_x_sigma,data_v_sigma)
 
-sampler,_,_,_ = model2.setup_sampler(200, 1, 1000)
+sampler,_,_,_ = model2.setup_sampler(500, 1000, 500)
 samples, flat_samples = sampler.get_chain(), sampler.get_chain(flat=True)
 
 label_list = model2.params_labels
