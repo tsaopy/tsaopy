a218d6b4300ca257b84c742143e4801f9aaa887e
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2022-05-26 11:14:03 -0300

    Update basictest.py

--- tests/basictest.py
+++ tests/basictest.py
@@ -50,3 +50,24 @@ for i in range(n_out):
     x[i] = x[i] + noise()*0.3
     v[i] = v[i] + noise()*0.2
 
+import tsaopy
+
+# load data
+data_t,data_x,data_v = t,x,v
+data_x_sigma,data_v_sigma = 0.15,0.15
+
+# priors
+
+x0_prior = tsaopy.tools.uniform_prior(0.7,1.3)
+v0_prior = tsaopy.tools.uniform_prior(-1.0,1.0)
+a1_prior = tsaopy.tools.uniform_prior(-5.0,5.0)
+b1_prior = tsaopy.tools.uniform_prior(0.0,5.0)
+    
+# parameters
+
+x0 = tsaopy.parameters.FittingParameter(1.0,'x0',1,x0_prior)
+v0 = tsaopy.parameters.FittingParameter(0.0,'v0',1,v0_prior)
+a1 = tsaopy.parameters.FittingParameter(0.0, 'a', 1, a1_prior)
+b1 = tsaopy.parameters.FittingParameter(0.5,'b',1,b1_prior)
+
+parameters = [x0,v0,a1,b1]
