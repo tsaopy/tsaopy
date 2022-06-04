edaee186974d816aa3bf57d011b7ca0d199e0e7b
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2022-06-04 20:39:57 -0300

    Update parameters.py

--- tsaopy/parameters.py
+++ tsaopy/parameters.py
@@ -1,37 +1,36 @@
 import sys
-from tsaopy.bendtools import test_var_is_number
+from tsaopy2.bendtools import test_var_is_number
 
 #                               PARAMETER CLASSES
 
 
 class Fixed:
     """
+    TSAOpy fixed parameter class.
 
-    This class represents a fixed tsaopy parameter object used to set up the
-    tsaopy model. It will tell the backend which term of the ODE this
-    coefficient belongs to, and how much its value is.
+    This class represents a fixed TSAOpy parameter object used to set up the
+    TSAOpy model.
 
-    I advice against using the Fixed class since it gives the backend less
+    It's adviced against using the Fixed class since it gives the backend less
     freedom to optimize the fit. If you are very confident in a certain
     parameter value you can use a Fitting parameter with a very narrow prior
     instead.
-
     """
 
     def __init__(self, value, ptype, index=None):
         """
-
         Initialice the instance.
 
         Args:
         value(int or float):  numerical value associated with the parameter.
         If parameter is Fixed it's the value assigned.
         ptype(str): string with the values ep, x0, v0, a, b, c, or f. Used to
-        identify if the parameter is an initial condition, or an ODE
-        coefficient.
-        index(int of int touple): optional for ep, x0, and v0 ptypes (does
-        nothing). It is necessary for a, b, c, and f ptypes. If not set for
-        those ptypes it will cause errors when building tsaopy models.
+        identify if the parameter is an initial condition, an ODE
+        coefficient, or another kind of parameter.
+        index(int of int touple): optional for ep, x0, v0, log_fx and log_fv
+        ptypes (does nothing). It is necessary for a, b, c, and f ptypes. If
+        not set for those ptypes it will cause errors when building TSAOpy
+        models.
             - for a and b parameters its the order of the term,
                 eg: b_2*x^2 => index = 2
             - for c parameters it is a touple with the order of x as first
@@ -41,7 +40,6 @@ class Fixed:
 
         Returns:
         tsaopy parameter object instance.
-
         """
         if not test_var_is_number(value):
             sys.exit("tsaopy params error: input value is not a number.")
@@ -53,30 +51,28 @@ class Fixed:
 
 class Fitting(Fixed):
     """
+    TSAOpy fitting parameter class.
 
-    This class creates a fitting tsaopy parameter object used to set up the
-    tsaopy model. It will tell the backend which term of the ODE this
-    coefficient belongs to, and what your initial estimate of its value is.
-    In addition to the 'value', 'ptype', and 'index' arguments you need to
-    provide a prior in the form of a normalized PDF. Methods to easily define
-    priors are given in the tools module.
-
+    This class creates a fitting TSAOpy parameter object used to set up the
+    TSAOpy model. In addition to the 'value', 'ptype', and 'index' arguments
+    it is necessary to provide a prior in the form of a normalized PDF. Methods
+    to easily define priors are given in the tools module.
     """
 
-    def __init__(self, value, ptype, index, prior):
+    def __init__(self, value, ptype, prior, index=None):
         """
-
         Initialice the instance.
 
         Args:
         value(int or float): estimation of the unknown variable that will be
         supplied to the initial values of the chain.
-        ptype(str): string with the values x0, v0, a, b, c, or f. Used to
-        identify if the parameter is an initial condition, or an ODE
-        coefficient.
-        index(int of int touple): optional for ep, x0, and v0 ptypes (does
-        nothing). It is necessary for a, b, c, and f ptypes. If not set for
-        those ptypes it will cause errors when building tsaopy models.
+        ptype(str): string with the values ep, x0, v0, a, b, c, or f. Used to
+        identify if the parameter is an initial condition, an ODE
+        coefficient, or another kind of parameter.
+        index(int of int touple): optional for ep, x0, v0, log_fx and log_fv
+        ptypes (does nothing). It is necessary for a, b, c, and f ptypes. If
+        not set for those ptypes it will cause errors when building TSAOpy
+        models.
             - for a and b parameters its the order of the term,
                 eg: b_2*x^2 => index = 2
             - for c parameters it is a touple with the order of x as first
@@ -88,8 +84,7 @@ class Fitting(Fixed):
         a number as argument.
 
         Returns:
-        tsaopy parameter object instance.
-
+        TSAOpy parameter object instance.
         """
         super().__init__(value, ptype, index)
         self.fixed = False
