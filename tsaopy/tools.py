d32a8e4115e1ebc3564b163f04665548e22e2ff9
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2022-06-05 21:58:02 -0300

    Update tools.py

--- tsaopy/tools.py
+++ tsaopy/tools.py
@@ -4,8 +4,57 @@ from math import sqrt
 import matplotlib.pyplot as plt
 import corner
 from emcee.autocorr import function_1d
+from scipy.stats import gaussian_kde
 
 from tsaopy.bendtools import test_var_is_number
+
+
+#                           Aux functions
+
+def mode(data, xmin=None, xmax=None, bw=None, xr_fr=.1):
+    """
+    Compute the mode of a 1D array of numerical values.
+
+    Compute the mode of an array of values using a kernel density estimation
+    method. The KDE method used is scipy.stats.gaussian_kde.
+
+    In multimodal distributions it should return the mode of the highest peak.
+
+    Parameters
+    ----------
+    data : array
+        Array containing the values.
+    xmin : float or int, optional
+        Lower bound for cutting off the values. The default is min(data).
+    xmax : float or int, optional
+        Upper bound for cutting off the values. The default is max(data).
+    bw : str, scalar or callable, optional
+        The method used to calculate the estimator bandwidth when calling
+        `scipy.stats.gaussian_kde`. The default is None (uses the default
+        method). See `scipy` docs for more info.
+    xr_fr : float or int, optional
+        Parameter used to define the KDE grid. The default is .1.
+
+    Returns
+    -------
+    float
+        Value of the mode.
+    """
+    if xmin is None:
+        xmin = np.min(data)
+    if xmax is None:
+        xmax = np.max(data)
+    # Define KDE limits.
+    x_rang = xr_fr * (xmax - xmin)
+    kde_x = np.mgrid[xmin - x_rang:xmax + x_rang:1000j]
+    try:
+        kernel_cl = gaussian_kde(data, bw_method=bw)
+        kde = np.reshape(kernel_cl(kde_x).T, kde_x.shape)
+    except np.linalg.LinAlgError:
+        kde = np.array([])
+
+    return kde_x[np.argmax(kde)]
+
 
 #                                   PRIOR CLASSES
 
@@ -24,14 +73,12 @@ class uniform_prior:
 
     def __init__(self, xmin, xmax):
         """
-        Initialice the instance.
-
-        Args:
-        xmin(int or float): lower bound of the uniform distribution.
-        xmax(int or float): upper bound of the uniform distribution.
-
-        Returns:
-        tsaopy pdf object instance.
+        Parameters
+        ----------
+        xmin : int or float
+            lower bound of the uniform distribution.
+        xmax : int or float
+            upper bound of the uniform distribution.
         """
         if not test_var_is_number(xmin) or not test_var_is_number(xmax):
             sys.exit("tsaopy priors error: input parameters are not real "
@@ -46,11 +93,14 @@ class uniform_prior:
         """
         Compute the probability of a real valued variable.
 
-        Args:
-        x(int or float)
+        Parameters
+        ----------
+        x : int or float
 
-        Returns:
-        p(x) as per the mathematical definition of the PDF.
+        Returns
+        -------
+        number
+            p(x) as per the mathematical definition of the PDF.
         """
         p = 1 if x < self.xmax and x > self.xmin else 0
         return p / (self.xmax - self.xmin)
@@ -70,14 +120,12 @@ class normal_prior:
 
     def __init__(self, x0, sigma):
         """
-        Initialice the instance.
-
-        Args:
-        x0(int or float): central value of the normal distribution.
-        simga(int or float): standard deviation of the normal distribution.
-
-        Returns:
-        tsaopy pdf object instance.
+        Parameters
+        ----------
+        x0 : int or float
+            central value of the normal distribution.
+        sigma : int or float
+            standard deviation of the normal distribution.
         """
         if not test_var_is_number(x0) or not test_var_is_number(sigma):
             sys.exit("tsaopy priors error: input parameters are not real "
@@ -111,12 +159,15 @@ def cornerplots(flat_samples, labels_list):
     value reported above the plot. The dashed grey lines mark the 16/84 and
     84/16 quantiles, which indicate the SD in a normal distribution.
 
-    Args:
-    flat_samples(array): flattened array of samples.
-    labels_list(list of strings): list with the label for each parameter of the
-    samples.
+    Parameters
+    ----------
+    flat_samples : array
+        flattened array of samples.
+    labels_list : list
+        list with a label(str) for each parameter of the samples.
 
-    Returns:
+    Returns
+    -------
     Displays created figures.
     """
     dim = len(labels_list)
@@ -144,14 +195,16 @@ def traceplots(samples, labels_list):
 
     Notice that in this case the samples object is not given flattened.
 
-    Args:
-    samples(array): non flattened array of samples.
-    labels_list(list of strings): list with the label for each parameter of the
-    samples.
+    Parameters
+    ----------
+    samples : array
+        non flattened array of samples.
+    labels_list : list
+        list with a label(str) for each parameter of the samples.
 
-    Returns:
+    Returns
+    -------
     Displays created figures.
-
     """
     dim = len(labels_list)
     fig, axes = plt.subplots(dim, figsize=(10, 7), dpi=100, sharex=True)
@@ -184,14 +237,16 @@ def autocplots(flat_samples, labels_list):
     you cannot trust that the chain has converged and therefore that your
     results are acceptable.
 
-    Args:
-    flat_samples(array): flattened array of samples.
-    labels_list(list of strings): list with the label for each parameter of the
-    samples.
+    Parameters
+    ----------
+    flat_samples : array
+        flattened array of samples.
+    labels_list : list
+        list with a label(str) for each parameter of the samples.
 
-    Returns:
+    Returns
+    -------
     Displays created figures.
-
     """
     dim, clen = len(labels_list), len(flat_samples)
     step_slice = clen // 100
