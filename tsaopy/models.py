a9d92c3f2bd5c27816082b5516a4f82c985fcf64
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2022-07-21 00:18:44 -0300

    Update models.py

--- tsaopy/models.py
+++ tsaopy/models.py
@@ -1,18 +1,59 @@
 """Main submodule of the library."""
 from tsaopy._f2pyauxmod import simulation, simulationv
 
+
+#           Aux stuff, raises etc
+class ModelInitException(Exception):
+    """Custom exception for Event instance init."""
+
+    def __init__(self, rtext):
+        """Edit init."""
+        msg = rtext + (' Exception ocurred while trying to instance a tsaopy '
+                       'Model object.')
+        super().__init__(msg)
+
+
 class Model:
     """Main object of the library."""
 
     def __init__(self, ode_coefs, events):
         """Do."""
-        pass
+        # run tests
+        for i in ode_coefs:
+            try:
+                if len(ode_coefs[i]) < 3:
+                    raise ModelInitException('ode coefs dict has less info '
+                                             'than expected. Remember to add '
+                                             'initial guess, prior, and index '
+                                             'for each coefficient.')
+                if len(ode_coefs[i]) > 3:
+                    raise ModelInitException('ode coefs dict has more info '
+                                             'than expected. Remember to only '
+                                             'add initial guess, prior, and '
+                                             'index for each coefficient.')
+                ilen = len(ode_coefs[i][2])
+                
+            # general exception when we don't know what happened
+            finally:
+                raise ModelInitException('ode coefs dict has something wrong.')
+
+        # a and b 1D vectors
+        self.aindex = ode_coefs['a'][-1]
+        self.alen, self.ashape = len(self.aindex), max(self.aindex)
+
+        self.bindex = ode_coefs['b'][-1]
+        self.blen, self.bshape = len(self.bindex), max(self.bindex)
+
+        # f vector
+        if 'f' not in ode_coefs:
+            self.using_f = False
+        elif 'f' in ode_coefs:
+            self.using_f = True
+            assert len
 
     def predict_sev(self, A, B, F, C, i):
         """Compute the prediction for a single event."""
-
-        use_ep = self.events[i].use_ep
-        if self.events[i].use_ep a
+        pass
 
     def predict(self, coords):
         """Compute the prediction for all events."""
@@ -33,4 +74,4 @@ class Model:
         for event in self.events:
             x0, v0 = coords[last_n], coords[last_n + 1]
             if event.use_ep:
-                ep = 
+                ep = coords[last_n + 2]
