38e4d2c08f62ca53901d21d431515374faba58bd
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2022-07-21 19:59:14 -0300

    Update models.py

--- tsaopy/models.py
+++ tsaopy/models.py
@@ -13,6 +13,7 @@ class ModelInitException(Exception):
         super().__init__(msg)
 
 
+#           tsaopy scripts
 class Model:
     """Main object of the library."""
 
@@ -52,7 +53,7 @@ class Model:
         elif 'f' in ode_coefs:
             self.using_f = True
 
-    def _ode_arrays(self, coords):
+    def _ode_arrays(self, ode_coefs):
         alen, blen, cn, cm = self.alen, self.blen, self.cn, self.cm
         A, B, F, C = (np.zeros(alen), np.zeros(blen), np.zeros(3),
                       np.zeros((cn, cm)))
@@ -60,29 +61,54 @@ class Model:
 
     def log_likelihood(self, coords):
         """Compute log likelihood for all events."""
-        A, B, F, C = self._ode_arrays(coords)
+        odecoefs_ndim = self.odecoefs_ndim
+        ode_coefs, event_params = (coords[:odecoefs_ndim],
+                                   coords[odecoefs_ndim:])
+
+        A, B, F, C = self._ode_arrays(ode_coefs)
 
         last_n = 0
         result = .0
         # iterate over all events
         for event in self.events:
-            x0v0 = coords[last_n:last_n + 2]
+            x0v0 = event_params[last_n:last_n + 2]
             last_n += 2
             if event.using_ep:
-                ep = coords[last_n]
+                ep = event_params[last_n]
                 last_n += 1
+            else:
+                ep = None
             if event.using_logfx:
-                logfx = coords[last_n]
+                logfx = event_params[last_n]
                 last_n += 1
+            else:
+                logfx = None
             if event.using_logfv:
-                logfv = coords[last_n]
+                logfv = event_params[last_n]
                 last_n += 1
+            else:
+                logfv = None
             result += event.log_likelihood(self, A, B, F, C, x0v0,
                                            ep, logfx, logfv)
 
     def log_prior(self, coords):
         """Compute log prior for all events."""
-        result = .0
+        odecoefs_ndim = self.odecoefs_ndim
+        odecoords = coords[:odecoefs_ndim]
+        result = 1
+        for i, p in enumerate(self.priors):
+            prob = p(odecoords[i])
+            if prob <= .0:
+                return -np.inf
+            result *= prob
+        result = np.log(result)
+
+        last_n = odecoefs_ndim
         # iterate over all events
         for event in self.events:
-            event_prior = 0
+            event_ndim = event.ndim
+            event_coords = coords[last_n:last_n + event_ndim]
+            last_n += event_ndim
+            result += event.log_prior(event_coords)
+
+        return result
